c7d84162f3c6582fcb3c08d2948e1248c58ed127
IzZumi228 <75075390+IzZumi228@users.noreply.github.com>
2025-07-22 16:40:20 -0400

    floor tiles implemented

--- code/level.py
+++ code/level.py
@@ -1,6 +1,9 @@
 import pygame 
 from settings import *
 from player import Player
+from sprites import Generic, Water
+from pytmx.util_pygame import load_pygame
+from support import *
 
 class Level:
 	def __init__(self):
@@ -9,15 +12,62 @@ class Level:
 		self.display_surface = pygame.display.get_surface()
 
 		# sprite groups
-		self.all_sprites = pygame.sprite.Group()
+		self.all_sprites = CameraGroup()
 
 
 		self.setup()
 
 	def setup(self):
+		tmx_data = load_pygame('data/map.tmx')
+
+		# house
+		for layer in ['HouseFloor', 'HouseFurnitureBottom']:
+			for x,y,surface in tmx_data.get_layer_by_name(layer).tiles():
+				Generic((x*TILE_SIZE, y*TILE_SIZE), surface, self.all_sprites, LAYERS['house bottom'])
+
+		for layer in ['HouseWalls', 'HouseFurnitureTop']:
+			for x,y,surface in tmx_data.get_layer_by_name(layer).tiles():
+				Generic((x*TILE_SIZE, y*TILE_SIZE), surface, self.all_sprites,)
+
+		# fence
+		for x, y, surface in tmx_data.get_layer_by_name('Fence').tiles():
+			Generic((x*TILE_SIZE, y*TILE_SIZE), surface, self.all_sprites,)
+
+		# water
+		water_frames = import_folder("graphics/water")
+		for x, y, surface in tmx_data.get_layer_by_name('Water').tiles():
+			Water((x*TILE_SIZE, y*TILE_SIZE), water_frames, self.all_sprites)
+
+
 		self.player = Player((640, 320), self.all_sprites)
+		Generic(
+			pos = (0,0),
+			surf = pygame.image.load("graphics/world/ground.png").convert_alpha(),
+			groups = self.all_sprites,
+			z = LAYERS['ground'])
 
 	def run(self,dt):
-		self.display_surface.fill('grey')
-		self.all_sprites.draw(self.display_surface)
+		self.all_sprites.custom_draw(self.player)
 		self.all_sprites.update(dt)
+
+
+
+class CameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.offset = pygame.math.Vector2()
+
+
+	def custom_draw(self, player):
+		self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
+
+		for layer in LAYERS.values():
+			for sprite in self.sprites():
+				if sprite.z == layer:
+					offset_rect = sprite.rect.copy()
+					offset_rect.center -= self.offset
+					self.display_surface.blit(sprite.image, offset_rect)
+
+
